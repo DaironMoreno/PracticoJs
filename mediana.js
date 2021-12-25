4eb90ad13f121debe04d1fcdf4a08417a2bd3593
DaironMoreno <dairon9@gmail.com>
2021-12-25 12:51:50 -0500

    calculo de media con funciones y ordenamiento de items en la lista

--- mediana.js
+++ mediana.js
@@ -1,11 +1,13 @@
-const lista1 = [
-    100,
-    200,
-    300,
-    400
-];
 
+const OrdenarArrays = (arr) =>
+{
+    arr.sort(function(a,b){
+        return a-b
+    });
+}
 const SumarListado = (lista) =>{
+    
+    
 
     const SumalistaAcumulada = lista.reduce(function(acumulado = 0,valoraSumar){
             return acumulado + valoraSumar;
@@ -13,31 +15,32 @@ const SumarListado = (lista) =>{
     const PromedioAritmetico = SumalistaAcumulada / lista.length;
     return PromedioAritmetico;
 }
-
-const mitad = parseInt(lista1.length / 2);
-
 const esPar = (valor) =>
-{
-    if(valor % 2 === 0)
     {
-        return true;
-    }else{
-        return false;
+        if(valor % 2 === 0)
+        {
+            return true;
+        }else{
+            return false;
+        }
     }
-}
 
-let mediana ;
-
-if(esPar(lista1.length))
-{
-    const valor1 = lista1[mitad - 1];
-    const valor2 = lista1[mitad];
-    const resultado = SumarListado([
-        valor1,
-        valor2
-    ]);
-    mediana = resultado;
+const CalcularMediana = (arr) => {
+    let lista = OrdenarArrays(arr);
+    let mediana ;
+    const mitad = parseInt(lista.length / 2);
     
-}else{
-    mediana = lista1[mitad];
+    if(esPar(lista.length))
+    {
+        const valor1 = lista[mitad - 1];
+        const valor2 = lista[mitad];
+        const resultado = SumarListado([
+            valor1,
+            valor2
+        ]);
+            return  mediana = resultado;
+        
+    }else{
+            return mediana = lista1[mitad];
+    }
 }
